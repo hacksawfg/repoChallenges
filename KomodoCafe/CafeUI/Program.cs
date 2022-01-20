52d9761266c889c304f72047d8d1f1a2fda13e0d
Findley Griffiths <findleygriffiths@gmail.com>
2022-01-20 14:49:30 -0500

    Checklist comments updated

--- KomodoCafe/CafeUI/Program.cs
+++ KomodoCafe/CafeUI/Program.cs
@@ -17,7 +17,7 @@ namespace CafeUI
 // Tasks
 // DONE - Create a class for menu items with properties, constructors, and fields
 // DONE - Create a class for the repository that houses the methods
-// Create a test class with coverage for repository functionality
+// DONE - Create a test class with coverage for repository functionality
 // DONE - Create a program that allows the cafe manager to [X] Add [X] Delete [X] See all items in the list
 
 // Version 2.0 notes
