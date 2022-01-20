6d2e348ba75bf90206a8882150d6a5f008a3cbb7
Findley Griffiths <findleygriffiths@gmail.com>
2022-01-20 10:07:13 -0500

    Adding some input validation

--- KomodoCafe/CafeUI/KomodoUI.cs
+++ KomodoCafe/CafeUI/KomodoUI.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CafeLibrary;
 
+
 namespace CafeUI
 {
     public class KomodoUI
@@ -12,6 +13,7 @@ namespace CafeUI
 
         public void Run()
         {
+            SeedContent();
             RunMenu();
         }
 
@@ -28,6 +30,7 @@ namespace CafeUI
                 Console.WriteLine("1. Create New Menu Item");
                 Console.WriteLine("2. Delete an Existing Menu Item");
                 Console.WriteLine("3. Display All Items on the Menu");
+                Console.WriteLine("4. Exit");
 
                 string userSelection = Console.ReadLine();
 
@@ -43,6 +46,15 @@ namespace CafeUI
                         // display all method
                         DisplayAllMenuItems();
                         break;
+                    case "4":
+                        Console.WriteLine("Goodbye");
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid selection: ");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
                 }
             }
         }
@@ -52,7 +64,7 @@ namespace CafeUI
             Console.Clear();
 
             MenuItem item = new MenuItem();
-
+            
             Console.Write("Enter the meal name: " );
             item.MealName = Console.ReadLine();
 
@@ -60,7 +72,7 @@ namespace CafeUI
             item.MealDescription = Console.ReadLine();
 
             Console.WriteLine("\nEnter the list of ingredients, separated by commas (i.e. hamburger, bread, etc.)");
-            string ingredients = Console.ReadLine().Replace(" ", "");
+            string ingredients = Console.ReadLine();
             string[] recipe = ingredients.Split(',');
             foreach (string ingredient in recipe)
             {
@@ -68,7 +80,18 @@ namespace CafeUI
             }
 
             Console.Write($"\nSet the price for {item.MealName}: $");
-            item.MealPrice = Convert.ToDecimal(Console.ReadLine());
+            string itemPriceInput = Console.ReadLine();
+            decimal validPrice;
+            bool checkPrice = decimal.TryParse(itemPriceInput, out validPrice);
+            if (checkPrice)
+            {
+                item.MealPrice = validPrice;
+            }
+            else
+            {
+                Console.Write("Please enter a valid price: $");
+                itemPriceInput = Console.ReadLine();
+            }    
 
             Console.Write("Is the meal vegetarian (Y/N)?: ");
             string vegetarian = Console.ReadLine().ToLower();
@@ -78,20 +101,24 @@ namespace CafeUI
                 case "y":
                     item.IsVegetarian = true;
                     break;
-                case "n":
+                default:
                     item.IsVegetarian = false;
                     break;
-            }   
+            }
+
+            _menu.AddMenuItem(item);   
         }
 
         private void DisplayMenuItem(MenuItem item)
         {
+            
+            string ingredientDisplay = string.Join(", ", item.MealIngredients);
             Console.WriteLine(
                             $"Menu Number #{item.MealNumber}. {item.MealName}\n" +
                             $"{item.MealDescription}\n" +
-                            $"Ingredients: {item.MealIngredients}\n" +
+                            $"Ingredients: {ingredientDisplay}\n" +
                             $"Price: ${item.MealPrice}\n" +
-                            $"Vegetarian: {item.IsVegetarian}" 
+                            $"Vegetarian: {item.IsVegetarian}\n" 
                             );
         }
 
@@ -107,6 +134,26 @@ namespace CafeUI
 
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
+        }
+
+        private void SeedContent()
+        {
+            string[] cheeseburgerIngred = {"ground beef", "cheddar cheese", "brioche bun", "lettuce", "tomato", "onion"};
+            List<string> cbIngred = new List<string>(cheeseburgerIngred);
+            MenuItem cheeseburger = new MenuItem(1, "Cheeseburger", "The All-American classic", cbIngred, 4.99m, false );
+            
+            string[] denverEggsIngred = {"eggs", "ham", "onion", "green pepper"};
+            List<string> denverIngred = new List<string>(denverEggsIngred);
+            MenuItem denverOmelette = new MenuItem(2, "Denver Omelette", "Delicious Breakfast", denverIngred, 3.99m, false);
+
+            string[] houseSaladIngred = {"iceberg lettuce", "tomato", "cucumber", "cheddar cheese", "croutons"};
+            List<string>houseIngred = new List<string>(houseSaladIngred);
+            MenuItem houseSalad = new MenuItem(3, "House Salad", "Healthy Alternative", houseIngred, 2.99m, true);
+
+            _menu.AddMenuItem(cheeseburger);
+            _menu.AddMenuItem(denverOmelette);
+            _menu.AddMenuItem(houseSalad);
+
         }
     }
 }
